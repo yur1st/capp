ae15c491c5af163638cd0e2604f50aac1dd85f2a
yur1st <okhvat@gmail.com>
2020-12-02 22:22:43 +0300

    Initial commit

---
+++ src/test/java/com/capp/tech/TechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capp.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

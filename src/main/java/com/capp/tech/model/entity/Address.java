66235592b7722add7957db9f5a7afdfd791f2b7b
yur1st <okhvat@gmail.com>
2020-12-03 22:32:07 +0300

    Add Address entity Add GuyWithAddressDTO

---
+++ src/main/java/com/capp/tech/model/entity/Address.java
@@ -0,0 +1,32 @@
+package com.capp.tech.model.entity;
+
+public class Address extends BaseEntity{
+
+    private String city;
+    private int postalIndex;
+    private String address;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPostalIndex() {
+        return postalIndex;
+    }
+
+    public void setPostalIndex(int postalIndex) {
+        this.postalIndex = postalIndex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}

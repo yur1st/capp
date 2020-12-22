9931e3193b67ad0597c8fbc43dc42ca51df323f7
yur1st <okhvat@gmail.com>
2020-12-23 01:26:50 +0300

    Add Fuse Theme

---
+++ ui/src/app/store/actions/fuse/dialog.actions.js
@@ -0,0 +1,17 @@
+export const OPEN_DIALOG = '[DIALOG] OPEN';
+export const CLOSE_DIALOG = '[DIALOG] CLOSE';
+
+
+export function closeDialog() {
+    return {
+        type: CLOSE_DIALOG
+    }
+}
+
+export function openDialog(options) {
+    return {
+        type: OPEN_DIALOG,
+        options
+    }
+}
+
